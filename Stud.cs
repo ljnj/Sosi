340bd440eab0fa572df543c7dbaaec19aba725ae
Murk <32967078+nydopustimname@users.noreply.github.com>
2019-03-25 23:28:21 +0300

    Add files via upload

--- Stud.cs
+++ Stud.cs
@@ -6,38 +6,33 @@ namespace ii_dinahyi_
 {
     class Stud
     {
-        public static void IsInMemory (Bitmap bmp, string name)
+        public static void IsInMemory(Bitmap bmp, string name)
         {
-            if (!File.Exists(name))
-            {
-                File.Create(name).Close();
-            }
+            if (!File.Exists(name)) File.Create(name).Close();
             string[] str = File.ReadAllLines(name);
             var arr = ObjToAnother.GetArrFromBMP(bmp);
-           
-            var temp = new char[2500];
-            var g = 0;
-            for (var i=0; i<arr.GetLength(0); i++)
-            {
-                for (var j=0; j<arr.GetLength(1); j++)
-                {
-                    temp[g] =Char.Parse( arr[j,i].ToString());
-                    g++;
-                }
-            }
-                var it = new double[str.Length];
+            
+            var it = new double[str.Length];
             for (var i = 0; i < str.Length; i++)
             {
                 char[] t = str[i].ToCharArray();
+                int h = 0, w = 0;
                 for (var j = 0; j < t.Length; j++)
                 {
-                    if (temp[j] == t[j])
+                    if (arr[w, h] == Double.Parse(t[j].ToString()))
                         it[i]++;
+                    if (w == 49)
+                    {
+                        h++;
+                        w = -1;
+                    }
+                    w++;
                 }
                 Console.WriteLine("----------------" + it[i] / 25 + "%-----------------------------------");
             }
+
             var isit = true;
-            for (var i=0; i<it.Length; i++)
+            for (var i = 0; i < it.Length; i++)
             {
 
                 if (it[i] / 2500 > 0.85)
